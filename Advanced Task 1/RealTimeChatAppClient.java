d2928efdfc11db81a7dca0b5f148df5c85bc5573
HarsavarthiniD <dharsavarthini16@gmail.com>
2024-11-27 20:16:41 +0530

    Add files via upload

---
+++ Advanced Task 1/RealTimeChatAppClient.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class RealTimeChatAppClient {
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 12345);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println("Connected to the chat server");
+            Thread receiveMessages = new Thread(() -> {
+                String serverMessage;
+                try {
+                    while ((serverMessage = in.readLine()) != null) {
+                        System.out.println(serverMessage);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            receiveMessages.start();
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String message = scanner.nextLine();
+                out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
